fdea5a863ec449c159db32c61318062641ac97c6
Miguel Costallat <48732887+CostallatP@users.noreply.github.com>
2021-04-03 11:14:12 -0300

    Add files via upload

--- projeto 1/estagio/src/components/_nav/Nav.vue
+++ projeto 1/estagio/src/components/_nav/Nav.vue
@@ -2,7 +2,7 @@
     <div class="barraTop">
 <nav class="marginPrincipal">
     <router-link to ="/professores">Professor</router-link>
-    <router-link to ="/alunos">Alunos</router-link>
+    <router-link to ="/todosalunos">Alunos</router-link>
     <router-link to ="/sobre">Sobre</router-link>
 </nav>
     </div>
@@ -20,10 +20,6 @@ background-color: rgb(230, 230, 230) ;
 }
 
 
-.marginPrincipal{
-
-
-}
 nav{
     padding: 20px 20px 20px 0px;
 }
